8a6707bd6e6864ece61e9d6ee8408e4a1457e267
Vyrus <bar9ain@gmail.com>
2018-02-19 00:34:55 +0700

    no message

--- src/main/java/vyrus/models/ModelChrisCringle.java
+++ src/main/java/vyrus/models/ModelChrisCringle.java
@@ -4,60 +4,68 @@ import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
 public class ModelChrisCringle extends ModelHuman {
-private ModelRenderer helmet, helmet1, helmet2, helmet3, helmet4, bodyArmor, bodyArmor1, bodyArmor2, bodyArmor3, bodyArmor4, bodyArmor5, bodyArmor6;
-public ModelChrisCringle() {
-super();
-this.bipedHeadwear.isHidden = true;
+	private ModelRenderer helmet, helmet1, helmet2, helmet3, helmet4, bodyArmor, bodyArmor1, bodyArmor2, bodyArmor3,
+			bodyArmor4, bodyArmor5, bodyArmor6;
 
-this.bipedBodyWear.isHidden = true;
+	public ModelChrisCringle() {
+		super();
+		this.bipedHeadwear.isHidden = true;
+		this.bipedBodyWear.isHidden = true;
+		this.bipedHead = new ModelRenderer(this, 0, 0);
+		this.bipedHead.addBox(-4F, -8.0F, -6.0F, 8, 8, 8);
+		this.bipedHead.setRotationPoint(0.0F, 0.0F, 0.0F);
 
-helmet = new ModelRenderer(this, 32,6);
-helmet.addBox(-4F, -10.0F, -4F, 8, 2, 8, 0F);
-this.bipedHead.addChild(helmet);
+		helmet = new ModelRenderer(this, 32, 6);
+		helmet.addBox(-4F, -10.0F, -4F, 8, 2, 8, 0F);
+		this.bipedHead.addChild(helmet);
 
-helmet1 = new ModelRenderer(this, 37,7);
-helmet1.addBox(-1F, -12.0F, -2F, 5, 1, 6, 0F);
-this.bipedHead.addChild(helmet1);
+		helmet1 = new ModelRenderer(this, 37, 7);
+		helmet1.addBox(-1F, -12.0F, -2F, 5, 1, 6, 0F);
+		this.bipedHead.addChild(helmet1);
 
-helmet2 = new ModelRenderer(this, 32,7);
-helmet2.addBox(4F, -12.0F, 1F, 1, 4, 2, 0F);
-this.bipedHead.addChild(helmet2);
+		helmet2 = new ModelRenderer(this, 32, 7);
+		helmet2.addBox(4F, -12.0F, 1F, 1, 4, 2, 0F);
+		this.bipedHead.addChild(helmet2);
 
-helmet3 = new ModelRenderer(this, 49,1);
-helmet3.addBox(4F, -8.0F, 2F, 2, 2, 2, 0F);
-this.bipedHead.addChild(helmet3);
+		helmet3 = new ModelRenderer(this, 49, 1);
+		helmet3.addBox(4F, -8.0F, 2F, 2, 2, 2, 0F);
+		this.bipedHead.addChild(helmet3);
 
-helmet4 = new ModelRenderer(this, 37,7);
-helmet4.addBox(-1F, -11.0F, -2F, 5, 1, 6, 0F);
-this.bipedHead.addChild(helmet4);
+		helmet4 = new ModelRenderer(this, 37, 7);
+		helmet4.addBox(-1F, -11.0F, -2F, 5, 1, 6, 0F);
+		this.bipedHead.addChild(helmet4);
 
-bodyArmor = new ModelRenderer(this, 24,2);
-bodyArmor.addBox(-4F, 0.0F, -4F, 8, 3, 1, 0F);
-this.bipedBody.addChild(bodyArmor);
+		bodyArmor = new ModelRenderer(this, 24, 2);
+		bodyArmor.addBox(-4F, 0.0F, -4F, 8, 3, 1, 0F);
+		this.bipedHead.addChild(bodyArmor);
 
-bodyArmor1 = new ModelRenderer(this, 16,40);
-bodyArmor1.addBox(-3F, 4.0F, -4F, 6, 7, 1, 0F);
-this.bipedBody.addChild(bodyArmor1);
+		bodyArmor3 = new ModelRenderer(this, 24, 0);
+		bodyArmor3.addBox(-3F, 3.0F, -4F, 6, 1, 1, 0F);
+		this.bipedHead.addChild(bodyArmor3);
 
-bodyArmor2 = new ModelRenderer(this, 16,34);
-bodyArmor2.addBox(-2F, 6.0F, -5F, 4, 5, 1, 0F);
-this.bipedBody.addChild(bodyArmor2);
+		bodyArmor1 = new ModelRenderer(this, 16, 40);
+		bodyArmor1.addBox(-3F, 4.0F, -4F, 6, 7, 1, 0F);
+		this.bipedBody.addChild(bodyArmor1);
 
-bodyArmor3 = new ModelRenderer(this, 24,0);
-bodyArmor3.addBox(-3F, 3.0F, -4F, 6, 1, 1, 0F);
-this.bipedBody.addChild(bodyArmor3);
+		bodyArmor2 = new ModelRenderer(this, 16, 34);
+		bodyArmor2.addBox(-2F, 6.0F, -5F, 4, 5, 1, 0F);
+		this.bipedBody.addChild(bodyArmor2);
 
-bodyArmor4 = new ModelRenderer(this, 30,38);
-bodyArmor4.addBox(-4F, 2.0F, -3F, 8, 9, 1, 0F);
-this.bipedBody.addChild(bodyArmor4);
+		bodyArmor4 = new ModelRenderer(this, 30, 38);
+		bodyArmor4.addBox(-4F, 2.0F, -3F, 8, 9, 1, 0F);
+		this.bipedBody.addChild(bodyArmor4);
 
-bodyArmor5 = new ModelRenderer(this, 24,6);
-bodyArmor5.addBox(-1F, 11.0F, -3F, 2, 1, 1, 0F);
-this.bipedBody.addChild(bodyArmor5);
+		bodyArmor5 = new ModelRenderer(this, 24, 6);
+		bodyArmor5.addBox(-1F, 11.0F, -3F, 2, 1, 1, 0F);
+		this.bipedBody.addChild(bodyArmor5);
 
-bodyArmor6 = new ModelRenderer(this, 30,32);
-bodyArmor6.addBox(-4F, 1.0F, 1F, 8, 5, 1, 0F);
-this.bipedBody.addChild(bodyArmor6);
+		bodyArmor6 = new ModelRenderer(this, 30, 32);
+		bodyArmor6.addBox(-4F, 1.0F, 1F, 8, 5, 1, 0F);
+		this.bipedBody.addChild(bodyArmor6);
+		
+		this.bipedRightArmwear = new ModelRenderer(this, 48, 32);
+		this.bipedRightArmwear.addBox(-3F, -2.5F, -2.0F, 4, 12, 4, 0.25F);
+		this.bipedRightArmwear.setRotationPoint(-5.0F, 2.5F, 0.0F);
 
 	}
 }
--- src/main/java/vyrus/models/ModelDishevelf.java
+++ src/main/java/vyrus/models/ModelDishevelf.java
@@ -4,56 +4,57 @@ import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
 public class ModelDishevelf extends ModelHuman {
-private ModelRenderer helmet, helmet1, helmet2, helmet3, helmet4, helmet5, helmet6;
-public ModelDishevelf() {
-super();
-this.bipedHeadwear.isHidden = true;
+	private ModelRenderer helmet, helmet1, helmet2, helmet3, helmet4, helmet5, helmet6;
 
-this.bipedLeftArm = new ModelRenderer(this, 32, 48);
-this.bipedLeftArm.setRotationPoint(0, 2.5F, 0);
-this.bipedLeftArm.addBox(-1, -2.5F, -2, 3, 12, 4, 0F);
+	public ModelDishevelf() {
+		super();
+		this.bipedHeadwear.isHidden = true;
 
-this.bipedRightArm = new ModelRenderer(this, 40, 16);
-this.bipedRightArm.setRotationPoint(0, 2.5F, 0);
-this.bipedRightArm.addBox(-2, -2.5F, -2, 3, 12, 4, 0F);
+		this.bipedLeftArm = new ModelRenderer(this, 32, 48);
+		this.bipedLeftArm.setRotationPoint(0, 2.5F, 0);
+		this.bipedLeftArm.addBox(-1, -2.5F, -2, 3, 12, 4, 0F);
 
-this.bipedLeftArmwear = new ModelRenderer(this, 48, 48);
-this.bipedLeftArmwear.setRotationPoint(0, 2.5F, 0);
-this.bipedLeftArmwear.addBox(-1, -2.5F, -2, 3, 12, 4, 0.25F);
+		this.bipedRightArm = new ModelRenderer(this, 40, 16);
+		this.bipedRightArm.setRotationPoint(0, 2.5F, 0);
+		this.bipedRightArm.addBox(-2, -2.5F, -2, 3, 12, 4, 0F);
 
-this.bipedRightArmwear = new ModelRenderer(this, 40, 32);
-this.bipedRightArmwear.setRotationPoint(0, 2.5F, 0);
-this.bipedRightArmwear.addBox(-2, -2.5F, -2, 3, 12, 4, 0.25F);
+		this.bipedLeftArmwear = new ModelRenderer(this, 48, 48);
+		this.bipedLeftArmwear.setRotationPoint(0, 2.5F, 0);
+		this.bipedLeftArmwear.addBox(-1, -2.5F, -2, 3, 12, 4, 0.25F);
 
-helmet = new ModelRenderer(this, 36,1);
-helmet.addBox(-3F, -11.0F, -3F, 6, 1, 6, 0F);
-this.bipedHead.addChild(helmet);
+		this.bipedRightArmwear = new ModelRenderer(this, 40, 32);
+		this.bipedRightArmwear.setRotationPoint(0, 2.5F, 0);
+		this.bipedRightArmwear.addBox(-2, -2.5F, -2, 3, 12, 4, 0.25F);
 
-helmet1 = new ModelRenderer(this, 32,0);
-helmet1.addBox(-4F, -10.0F, -4F, 8, 2, 8, 0F);
-this.bipedHead.addChild(helmet1);
+		helmet = new ModelRenderer(this, 36, 1);
+		helmet.addBox(-3F, -11.0F, -3F, 6, 1, 6, 0F);
+		this.bipedHead.addChild(helmet);
 
-helmet2 = new ModelRenderer(this, 30,1);
-helmet2.addBox(4F, -4.0F, -1F, 2, 1, 1, 0F);
-this.bipedHead.addChild(helmet2);
+		helmet1 = new ModelRenderer(this, 32, 0);
+		helmet1.addBox(-4F, -10.0F, -4F, 8, 2, 8, 0F);
+		this.bipedHead.addChild(helmet1);
 
-helmet3 = new ModelRenderer(this, 32,3);
-helmet3.addBox(3F, -3.0F, -1F, 3, 1, 1, 0F);
-this.bipedHead.addChild(helmet3);
+		helmet2 = new ModelRenderer(this, 30, 1);
+		helmet2.addBox(4F, -4.0F, -1F, 2, 1, 1, 0F);
+		this.bipedHead.addChild(helmet2);
 
-helmet4 = new ModelRenderer(this, 24,1);
-helmet4.addBox(-6F, -4.0F, -1F, 2, 1, 1, 0F);
-helmet4.mirror = true;
-this.bipedHead.addChild(helmet4);
+		helmet3 = new ModelRenderer(this, 32, 3);
+		helmet3.addBox(4F, -3.0F, -1F, 3, 1, 1, 0F);
+		this.bipedHead.addChild(helmet3);
 
-helmet5 = new ModelRenderer(this, 24,3);
-helmet5.addBox(-7F, -3.0F, -1F, 3, 1, 1, 0F);
-helmet5.mirror = true;
-this.bipedHead.addChild(helmet5);
+		helmet4 = new ModelRenderer(this, 24, 1);
+		helmet4.addBox(-6F, -4.0F, -1F, 2, 1, 1, 0F);
+		helmet4.mirror = true;
+		this.bipedHead.addChild(helmet4);
 
-helmet6 = new ModelRenderer(this, 24,5);
-helmet6.addBox(-1F, -3.0F, -6F, 2, 1, 2, 0F);
-this.bipedHead.addChild(helmet6);
+		helmet5 = new ModelRenderer(this, 24, 3);
+		helmet5.addBox(-7F, -3.0F, -1F, 3, 1, 1, 0F);
+		helmet5.mirror = true;
+		this.bipedHead.addChild(helmet5);
+
+		helmet6 = new ModelRenderer(this, 24, 5);
+		helmet6.addBox(-1F, -3.0F, -6F, 2, 1, 2, 0F);
+		this.bipedHead.addChild(helmet6);
 
 	}
 }
